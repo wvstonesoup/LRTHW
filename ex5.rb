ff170af6cbeff6da9c0b2b2dd320223404a91874
Sharon Smith <Sharon Smith>
2016-08-22 23:10:58 -0400

    this is neater appearance dif math

--- ex5.rb
+++ ex5.rb
@@ -1,17 +1,17 @@
 name = 'Zed A. Shaw'
-age = 35 # not a lie in 2009
-height = 74 # inches
-weight = 180  # lbs
+age = 35  # not a lie in 2009
+height = 74 * 5/2 # inches
+weight = 180 * 2 # lbs
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
 
 puts "Let's talk about #{name}."
-puts "He's #{height * 2.54} centimeters tall."
-puts "He's #{weight * 2.205} klograms heavy."
+puts "He's #{height} centimeters tall."
+puts "He's #{weight} kilograms heavy."
 puts "Actually that's not too heavy."
 puts "He's got #{eyes} eyes and #{hair} hair."
 puts "His teeth are usually #{teeth} depending on the coffee."
 
 # this line is tricky, try to get it exactly right
-puts "If I add #{age}, #{height * 2.54}, and #{weight * 2.205} I get #{age + height + weight}."
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
