36cce4511fa81c94337f223daff9b1cfaff4cf6f
Sharon Smith <Sharon Smith>
2016-08-22 23:29:33 -0400

    with #s comments

--- ex6.rb
+++ ex6.rb
@@ -1,5 +1,5 @@
-types_of_people = 10
-x = "There are #{types_of_people} types of people."
+types_of_people = 10 #binary digits
+x = "There are #{types_of_people} types of people." #string in string
 binary = "binary"
 do_not = "don't"
 y = "Those who know #{binary} and those who #{do_not}."
@@ -7,8 +7,8 @@ y = "Those who know #{binary} and those who #{do_not}."
 puts x
 puts y
 
-puts "I said: #{x}."
-puts "I also said: '#{y}'."
+puts "I said: #{x}." #string in string
+puts "I also said: '#{y}'." #string in string
 
 hilarious = false
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
