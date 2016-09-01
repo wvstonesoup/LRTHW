5600d32eb98bd6128705a190e57f4f518543e434
Sharon Smith <Sharon Smith>
2016-09-01 11:49:08 -0400

    my own get.chomp form

---
+++ ex11drill.rb
@@ -0,0 +1,8 @@
+print "What kind of car do you have? "
+car = gets.chomp
+print "What state is it licensed in? "
+state = gets.chomp
+print "How many miles do you drive every week? "
+miles = gets.chomp
+
+puts "So, you drive a #{car} with #{state} plates about #{miles} miles a week."
