1a1438563aedf2e15dfe972316f1f0f8927cfc1f
Sharon Smith <Sharon Smith>
2016-09-04 11:25:03 -0400

    added second variable to argv

---
+++ ex14.rb
@@ -0,0 +1,22 @@
+first, second = ARGV # gets the first argument
+prompt = '> '
+
+puts "Hi #{first} #{second}"
+puts "I'd like to ask you a few questions."
+puts "Do you like me #{first}? "
+puts prompt
+likes = $stdin.gets.chomp
+
+puts "Where do you live #{first}? "
+puts prompt
+lives = $stdin.gets.chomp
+
+# a comma for puts is like using it twice
+puts "What kind of computer do you have? ", prompt
+computer = $stdin.gets.chomp
+
+puts """
+Alright, so you said #{likes} about liking me.
+You live in #{lives}.  Not sure where that is.
+And you have a #{computer} computer.  Nice.
+"""
