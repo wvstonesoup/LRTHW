6365ef9b26989d006ce215845fe4c52f7b415a0f
Sharon Smith <Sharon Smith>
2016-09-04 15:09:13 -0400

    use a semicolon to combine 2 lines of code

--- ex17.rb
+++ ex17.rb
@@ -3,8 +3,7 @@ from_file, to_file = ARGV
 puts "Copying from #{from_file} to #{to_file}"
 
 # we could do these two on one line, how?
-in_file = open(from_file)
-indata = in_file.read
+in_file = open(from_file); indata = in_file.read
 
 puts "The input file is #{indata.length} bytes long"
 
