264e3a37bce4521c2e404c8f68087cae3f900d73
Sharon Smith <Sharon Smith>
2016-09-04 16:10:03 -0400

    learn functions and adjust output print to screen

---
+++ ex17a.rb
@@ -0,0 +1,10 @@
+echo "This is a test file." > test.txt
+cat test.txt
+This is a test file.
+ruby ex17.rb test.txt new_file.txt
+Copying from test.txt to new_file.txt
+The input file is 21 bytes long
+Does the output file exist? false
+Ready, hit RETURN to continue, CTRL-C to abort.
+
+Alright, all done.
